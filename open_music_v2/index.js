3e6f0194fa802915cdea993922cd3ccd52f3e049
Moh Amilin <amilinmoh99@gmail.com>
2021-07-10 15:03:52 +0700

    update : register plugin user

--- open_music_v2/index.js
+++ open_music_v2/index.js
@@ -1,14 +1,27 @@
 require('dotenv').config()
 
 const Hapi = require('@hapi/hapi')
+
+/**
+ * for song
+ */
 const music = require('./src/api/songs')
 // const MusicService = require("./src/services/inMemory/MusicService");
-const MusicService = require('./src/services/postgreSQL/MusicService')
-
+const MusicService = require('./src/services/postgreSQL/musicService')
 const musicValidator = require('./src/validators/music')
+
+/**
+ * for user
+ */
+
+const user = require('./src/api/users')
+const UserService = require('./src/services/postgreSQL/userService')
+const userValidator = require('./src/validators/user')
 
 const init = async () => {
   const musicService = new MusicService()
+  const userService = new UserService()
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -21,13 +34,22 @@ const init = async () => {
     },
   })
 
-  await server.register({
-    plugin: music,
-    options: {
-      service: musicService,
-      validator: musicValidator,
+  await server.register([
+    {
+      plugin: music,
+      options: {
+        service: musicService,
+        validator: musicValidator,
+      },
     },
-  })
+    {
+      plugin: user,
+      options: {
+        service: userService,
+        validator: userValidator,
+      },
+    },
+  ])
 
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
