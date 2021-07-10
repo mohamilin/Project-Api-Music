c442f884da89ff62db6ae703233bd0aa2e9a6857
Moh Amilin <amilinmoh99@gmail.com>
2021-07-10 15:03:04 +0700

    update : add handler and route for user

---
+++ open_music_v2/src/api/users/handler.js
@@ -0,0 +1,85 @@
+const ClientError = require('../../exceptions/clientError')
+
+class UserHandler {
+  constructor(service, validator) {
+    this._service = service
+    this._validataor = validator
+
+    this.addUserHandler = this.addUserHandler.bind(this)
+    this.getUserByIdHandler = this.getUserByIdHandler.bind(this)
+  }
+
+  async addUserHandler(req, h) {
+    try {
+      await this._validataor.validationUserPayload(req.payload)
+      const { username, password, fullname } = req.payload
+      const userId = await this._service.addUser({
+        username,
+        password,
+        fullname,
+      })
+      const response = h.response({
+        status: 'success',
+        message: 'User berhasil ditambahkan',
+        data: {
+          userId,
+        },
+      })
+
+      response.code(201)
+      return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+
+  async getUserByIdHandler(req, h) {
+    try {
+      const { userId } = req.params
+      const user = await this._service.getUserByIdHandler(userId)
+      const response = h.response({
+        status: 'success',
+        data: {
+          user,
+        },
+      })
+      response.code(201)
+      return response
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        })
+        response.code(error.statusCode)
+        return response
+      }
+
+      // server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      })
+      response.code(500)
+      console.error(error)
+      return response
+    }
+  }
+}
+
+module.exports = UserHandler
