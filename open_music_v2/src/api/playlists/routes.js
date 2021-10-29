c57945f59d0cc2cac099f6ad25a8e9667acc75d3
Moh Amilin <amilinmoh99@gmail.com>
2021-10-29 20:04:38 +0700

    add : playlist

---
+++ open_music_v2/src/api/playlists/routes.js
@@ -0,0 +1,60 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/playlists',
+    handler: handler.addPlaylistHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists',
+    handler: handler.getPlaylistHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{playlistId}',
+    handler: handler.deletePlaylistHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'POST',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.addSongHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.getSongHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'DELETE',
+    path: '/playlists/{playlistId}/songs',
+    handler: handler.deleteSongHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+  {
+    method: 'GET',
+    path: '/users',
+    handler: handler.getUsernameHandler,
+    options: {
+      auth: 'open_music',
+    },
+  },
+];
+
+module.exports = routes;
