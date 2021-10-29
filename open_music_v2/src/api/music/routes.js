62d503fb30a1fe79ebd2fe8d03036ce538d07c31
Moh Amilin <amilinmoh99@gmail.com>
2021-10-29 20:04:46 +0700

    add : music

---
+++ open_music_v2/src/api/music/routes.js
@@ -0,0 +1,29 @@
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/songs',
+    handler: handler.addMusicHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs',
+    handler: handler.getMusicHandler,
+  },
+  {
+    method: 'GET',
+    path: '/songs/{songId}',
+    handler: handler.getMusicIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/songs/{songId}',
+    handler: handler.editMusicHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/songs/{songId}',
+    handler: handler.deleteMusicHandler,
+  },
+];
+
+module.exports = routes;
