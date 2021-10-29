a7b7127df65d780eeb80abd2989d6daf9d9a9332
Moh Amilin <amilinmoh99@gmail.com>
2021-10-29 20:14:29 +0700

    add : entry point

--- open_music_v2/index.js
+++ open_music_v2/index.js
@@ -1,27 +1,51 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const Hapi = require('@hapi/hapi')
+const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
-/**
- * for song
- */
-const music = require('./src/api/songs')
+const music = require('./src/api/music');
 // const MusicService = require("./src/services/inMemory/MusicService");
-const MusicService = require('./src/services/postgreSQL/musicService')
-const musicValidator = require('./src/validators/music')
+const MusicService = require('./src/services/postgreSQL/MusicService');
+const musicValidator = require('./src/validators/music');
 
 /**
  * for user
  */
 
-const user = require('./src/api/users')
-const UserService = require('./src/services/postgreSQL/userService')
-const userValidator = require('./src/validators/user')
+const user = require('./src/api/users');
+const UserService = require('./src/services/postgreSQL/UserService');
+const userValidator = require('./src/validators/user');
+
+/**
+ * for Authentication
+ */
+
+const authenticate = require('./src/api/authenticate');
+const AuthService = require('./src/services/postgreSQL/AuthService');
+const TokenManager = require('./src/token/tokenManager');
+const AuthenticationsValidator = require('./src/validators/auth');
+
+/**
+ * for Playlist
+ */
+
+const playlist = require('./src/api/playlists');
+const PlaylistService = require('./src/services/postgreSQL/PlaylistService');
+const playlistValidator = require('./src/validators/playlist');
+
+/**
+ * for collaborations
+ */
+const collaboration = require('./src/api/collaboration');
+const CollaborationService = require('./src/services/postgreSQL/CollaborationService');
+const collaborationValidator = require('./src/validators/collab');
 
 const init = async () => {
-  const musicService = new MusicService()
-  const userService = new UserService()
-
+  const musicService = new MusicService();
+  const userService = new UserService();
+  const authenticationsService = new AuthService();
+  const playlistService = new PlaylistService();
+  const collabService = new CollaborationService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -32,8 +56,41 @@ const init = async () => {
         origin: ['*'],
       },
     },
-  })
+  });
 
+  /**
+   * registrasi plugin eksternal
+   */
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  /**
+   * Definition for authentication jwt
+   */
+
+  // mendefinisikan strategy autentikasi jwt
+  server.auth.strategy('open_music', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
+  /**
+   * registrasi plugin internal
+   */
   await server.register([
     {
       plugin: music,
@@ -49,10 +106,36 @@ const init = async () => {
         validator: userValidator,
       },
     },
-  ])
+    {
+      plugin: authenticate,
+      options: {
+        // authService: authenticationsService,
+        // service: userService,
+        authenticationsService,
+        userService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
+    },
+    {
+      plugin: playlist,
+      options: {
+        service: playlistService,
+        validator: playlistValidator,
+      },
+    },
+    {
+      plugin: collaboration,
+      options: {
+        collabService,
+        musicService,
+        validator: collaborationValidator,
+      },
+    },
+  ]);
 
-  await server.start()
-  console.log(`Server berjalan pada ${server.info.uri}`)
-}
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
-init()
+init();
